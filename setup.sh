2359bf71117aa4aa6e9127a25f8bb87decb5194e
chuksmomoh2003 <69574802+chuksmomoh2003@users.noreply.github.com>
2021-12-11 18:52:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
